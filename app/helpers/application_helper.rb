7521167457bfab0c987c89029604a83266a68094
ber8749 <ber8749@gmail.com>
2016-01-04 19:08:10 -0800

    Clean up views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def dropdown_toggle_link(name)
+    link_to "#{name} <span class='carat'></span>".html_safe, '#', class: 'dropdown-toggle',
+            data: { toggle: 'dropdown' }, role: 'button', aria: { haspopup: 'true', expanded: 'false' }
+  end
 end
